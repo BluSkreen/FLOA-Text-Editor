82c5a0ff8e556f266e26879902eabdc1fc4bb9f6
BluSkreen <jasonduran303@gmail.com>
2023-01-11 16:23:24 -0700

    config name changes, deploying to heroku

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'JATE'
       }),
      
       // Injects our custom service worker
@@ -37,9 +37,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'JATE by blu',
+        short_name: 'JATE',
+        description: 'Simple, Clean, Text Editor',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
