155e3d431717300778dbd25fb2788c00cd07fb3d
BluSkreen <jasonduran303@gmail.com>
2023-01-11 16:59:19 -0700

    webpack production mode

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,12 +8,10 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: 'production',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js',
-      header: './src/js/header.js',
-      editor: './src/js/editor.js'
     },
     output: {
       filename: '[name].bundle.js',
